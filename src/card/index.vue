ba5176df0ce29ea167330c5b7289ae3e50265016
Charles Liu <superlc0413@163.com>
2019-04-16 00:39:17 +0800

    123

--- src/card/index.vue
+++ src/card/index.vue
@@ -5,50 +5,58 @@
       <li class="row1 tc">共有{{allList.length}}次报名</li>
       <li
         class="tc"
-      >单打报名{{singleList.length}}次, 双打报名{{coupeList.length}}次，家庭报名{{familyList.length}}次</li>
+      >单打报名{{singleList.length}}次, 双打报名{{coupeList.length/2}}次，家庭报名{{familyList.length/2}}次</li>
     </ul>
     <el-tabs class="main-tab" v-model="displayMode">
+      <el-tab-pane name="all" label="全部">
+        <el-table class="user-table" stripe :data="allList">
+          <el-table-column width="100px" prop="name" label="姓名"></el-table-column>
+          <el-table-column width="75px" prop="age" label="年龄"></el-table-column>
+          <el-table-column width="75px" prop="sex" label="性别"></el-table-column>
+          <el-table-column width="200px" prop="idCard" label="身份证号"></el-table-column>
+          <el-table-column width="150px" prop="phone" label="电话"></el-table-column>
+          <el-table-column width="100px" prop="type" label="报名类型"></el-table-column>
+          <el-table-column width="75px" prop="groups" label="组别"></el-table-column>
+          <el-table-column prop="partNo" label="报名编号"></el-table-column>
+          <!-- <el-table-column width="100px" prop="price" label="价格"></el-table-column> -->
+        </el-table>
+      </el-tab-pane>
       <el-tab-pane name="single" label="单人报名">
         <el-table class="user-table" stripe :data="singleList">
-          <el-table-column width="125px" prop="name" label="姓名"></el-table-column>
+          <el-table-column width="100px" prop="name" label="姓名"></el-table-column>
           <el-table-column width="75px" prop="age" label="年龄"></el-table-column>
-          <el-table-column prop="idCard" label="身份证号"></el-table-column>
-          <el-table-column prop="phone" label="电话"></el-table-column>
-          <el-table-column width="125px" prop="type" label="报名类型"></el-table-column>
-          <el-table-column prop="groups" label="组别"></el-table-column>
+          <el-table-column width="75px" prop="sex" label="性别"></el-table-column>
+          <el-table-column width="200px" prop="idCard" label="身份证号"></el-table-column>
+          <el-table-column width="150px" prop="phone" label="电话"></el-table-column>
+          <el-table-column width="100px" prop="type" label="报名类型"></el-table-column>
+          <el-table-column width="75px" prop="groups" label="组别"></el-table-column>
+          <el-table-column prop="partNo" label="报名编号"></el-table-column>
           <!-- <el-table-column width="100px" prop="price" label="价格"></el-table-column> -->
         </el-table>
       </el-tab-pane>
       <el-tab-pane name="coupe" label="双人报名">
         <el-table class="user-table" stripe :data="coupeList">
-          <el-table-column width="125px" prop="name" label="姓名"></el-table-column>
+          <el-table-column width="100px" prop="name" label="姓名"></el-table-column>
           <el-table-column width="75px" prop="age" label="年龄"></el-table-column>
-          <el-table-column prop="idCard" label="身份证号"></el-table-column>
-          <el-table-column prop="phone" label="电话"></el-table-column>
-          <el-table-column width="125px" prop="type" label="报名类型"></el-table-column>
-          <el-table-column prop="groups" label="组别"></el-table-column>
+          <el-table-column width="75px" prop="sex" label="性别"></el-table-column>
+          <el-table-column width="200px" prop="idCard" label="身份证号"></el-table-column>
+          <el-table-column width="150px" prop="phone" label="电话"></el-table-column>
+          <el-table-column width="100px" prop="type" label="报名类型"></el-table-column>
+          <el-table-column width="75px" prop="groups" label="组别"></el-table-column>
+          <el-table-column prop="partNo" label="报名编号"></el-table-column>
           <!-- <el-table-column width="100px" prop="price" label="价格"></el-table-column> -->
         </el-table>
       </el-tab-pane>
       <el-tab-pane name="family" label="家庭报名">
         <el-table class="user-table" stripe :data="familyList">
-          <el-table-column width="125px" prop="name" label="姓名"></el-table-column>
+          <el-table-column width="100px" prop="name" label="姓名"></el-table-column>
           <el-table-column width="75px" prop="age" label="年龄"></el-table-column>
-          <el-table-column prop="idCard" label="身份证号"></el-table-column>
-          <el-table-column prop="phone" label="电话"></el-table-column>
-          <el-table-column width="125px" prop="type" label="报名类型"></el-table-column>
-          <el-table-column prop="groups" label="组别"></el-table-column>
-          <!-- <el-table-column width="100px" prop="price" label="价格"></el-table-column> -->
-        </el-table>
-      </el-tab-pane>
-      <el-tab-pane name="all" label="全部">
-        <el-table class="user-table" stripe :data="allList">
-          <el-table-column width="125px" prop="name" label="姓名"></el-table-column>
-          <el-table-column width="75px" prop="age" label="年龄"></el-table-column>
-          <el-table-column prop="idCard" label="身份证号"></el-table-column>
-          <el-table-column prop="phone" label="电话"></el-table-column>
-          <el-table-column width="125px" prop="type" label="报名类型"></el-table-column>
-          <el-table-column prop="groups" label="组别"></el-table-column>
+          <el-table-column width="75px" prop="sex" label="性别"></el-table-column>
+          <el-table-column width="200px" prop="idCard" label="身份证号"></el-table-column>
+          <el-table-column width="150px" prop="phone" label="电话"></el-table-column>
+          <el-table-column width="100px" prop="type" label="报名类型"></el-table-column>
+          <el-table-column width="75px" prop="groups" label="组别"></el-table-column>
+          <el-table-column prop="partNo" label="报名编号"></el-table-column>
           <!-- <el-table-column width="100px" prop="price" label="价格"></el-table-column> -->
         </el-table>
       </el-tab-pane>
@@ -57,7 +65,7 @@
 </template>
 <script>
 const participantMap = {};
-// const prices = [88, 118];
+const prices = [120, 180, 80];
 
 export default {
   data() {
@@ -66,7 +74,7 @@ export default {
       coupeList: [],
       singleList: [],
       familyList: [],
-      displayMode: "single",
+      displayMode: "all",
       ages: [
         // { start: 0, end: 10 }
       ],
@@ -80,12 +88,14 @@ export default {
       if (arr instanceof Array) {
         const refinedData = arr.map(_ => {
           const newDatum = {
+            partNo: _.partNo,
+            type: _.type,
             name: _.name,
             phone: _.phone,
-            type: _.type,
+            sex: _.sex,
             groups: _.groups,
-            age: _.chileAge || _.age,
-            idCard: _.childIdCard || _.idCard
+            age: _.age,
+            idCard: _.idCard
           };
           participantMap[newDatum.idCard] = newDatum;
           return newDatum;
@@ -94,19 +104,19 @@ export default {
         refinedData.forEach(_ => {
           this.allList.push(_);
           if (_.type == "单打") {
-            // _.price = prices[1];
+            _.price = prices[1];
             this.singleList.push(_);
           } else if (_.type == "双打") {
-            // _.price = prices[0];
+            _.price = prices[0];
             this.coupeList.push(_);
           } else {
-            // _.price = prices[0];
+            _.price = prices[2];
             this.familyList.push(_);
           }
         });
       }
     },
-    getEnrollData(start = 1) {
+    getEnrollData(start = 0) {
       this.$http.post("/joinlist_bm.php", { start }).then(res => {
         if (res && res.status == 200 && res.data && res.data.length > 0) {
           this.processData(res.data);
