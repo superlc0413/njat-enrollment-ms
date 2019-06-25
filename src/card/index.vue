57a3040d5a3cce0a5fd55a5ef26a4f8100ad1106
Charles Liu <superlc0413@163.com>
2019-06-26 04:47:39 +0800

    123

--- src/card/index.vue
+++ src/card/index.vue
@@ -1,63 +1,36 @@
 <template>
   <div>
-    <div class="title tc">青少年羽毛球比赛报名情况</div>
+    <div class="title tc">亲子跑报名情况</div>
     <ul class="total-count">
       <li class="row1 tc">共有{{allList.length}}次报名</li>
-      <li
-        class="tc"
-      >单打报名{{singleList.length}}次, 双打报名{{coupeList.length/2}}次，家庭报名{{familyList.length/2}}次</li>
     </ul>
     <el-tabs class="main-tab" v-model="displayMode">
       <el-tab-pane name="all" label="全部">
         <el-table class="user-table" stripe :data="allList">
-          <el-table-column width="100px" prop="name" label="姓名"></el-table-column>
-          <el-table-column width="75px" prop="age" label="年龄"></el-table-column>
-          <el-table-column width="75px" prop="sex" label="性别"></el-table-column>
-          <el-table-column width="200px" prop="idCard" label="身份证号"></el-table-column>
-          <el-table-column width="150px" prop="phone" label="电话"></el-table-column>
-          <el-table-column width="100px" prop="type" label="报名类型"></el-table-column>
-          <el-table-column width="75px" prop="groups" label="组别"></el-table-column>
-          <el-table-column prop="partNo" label="报名编号"></el-table-column>
-          <!-- <el-table-column width="100px" prop="price" label="价格"></el-table-column> -->
+          <el-table-column prop="parentName" width="110px" label="家长姓名"></el-table-column>
+          <el-table-column prop="parentAge" width="110px" label="家长年龄"></el-table-column>
+          <el-table-column prop="parentIdCard" label="家长身份证"></el-table-column>
+          <el-table-column prop="parentPhone" label="家长手机号"></el-table-column>
+          <el-table-column prop="childName" width="110px" label="孩子姓名"></el-table-column>
+          <el-table-column prop="childAge" width="110px" label="孩子年龄"></el-table-column>
+          <el-table-column prop="childIdCard" label="孩子身份证"></el-table-column>
+          <el-table-column prop="childPhone" label="孩子手机号"></el-table-column>
         </el-table>
       </el-tab-pane>
-      <el-tab-pane name="single" label="单人报名">
-        <el-table class="user-table" stripe :data="singleList">
-          <el-table-column width="100px" prop="name" label="姓名"></el-table-column>
-          <el-table-column width="75px" prop="age" label="年龄"></el-table-column>
-          <el-table-column width="75px" prop="sex" label="性别"></el-table-column>
-          <el-table-column width="200px" prop="idCard" label="身份证号"></el-table-column>
-          <el-table-column width="150px" prop="phone" label="电话"></el-table-column>
-          <el-table-column width="100px" prop="type" label="报名类型"></el-table-column>
-          <el-table-column width="75px" prop="groups" label="组别"></el-table-column>
-          <el-table-column prop="partNo" label="报名编号"></el-table-column>
-          <!-- <el-table-column width="100px" prop="price" label="价格"></el-table-column> -->
+      <el-tab-pane name="parent" label="家长">
+        <el-table class="user-table" stripe :data="parentlist">
+          <el-table-column prop="parentName" label="家长姓名"></el-table-column>
+          <el-table-column prop="parentAge" label="家长年龄"></el-table-column>
+          <el-table-column prop="parentIdCard" label="家长身份证"></el-table-column>
+          <el-table-column prop="parentPhone" label="家长手机号"></el-table-column>
         </el-table>
       </el-tab-pane>
-      <el-tab-pane name="coupe" label="双人报名">
-        <el-table class="user-table" stripe :data="coupeList">
-          <el-table-column width="100px" prop="name" label="姓名"></el-table-column>
-          <el-table-column width="75px" prop="age" label="年龄"></el-table-column>
-          <el-table-column width="75px" prop="sex" label="性别"></el-table-column>
-          <el-table-column width="200px" prop="idCard" label="身份证号"></el-table-column>
-          <el-table-column width="150px" prop="phone" label="电话"></el-table-column>
-          <el-table-column width="100px" prop="type" label="报名类型"></el-table-column>
-          <el-table-column width="75px" prop="groups" label="组别"></el-table-column>
-          <el-table-column prop="partNo" label="报名编号"></el-table-column>
-          <!-- <el-table-column width="100px" prop="price" label="价格"></el-table-column> -->
-        </el-table>
-      </el-tab-pane>
-      <el-tab-pane name="family" label="家庭报名">
-        <el-table class="user-table" stripe :data="familyList">
-          <el-table-column width="100px" prop="name" label="姓名"></el-table-column>
-          <el-table-column width="75px" prop="age" label="年龄"></el-table-column>
-          <el-table-column width="75px" prop="sex" label="性别"></el-table-column>
-          <el-table-column width="200px" prop="idCard" label="身份证号"></el-table-column>
-          <el-table-column width="150px" prop="phone" label="电话"></el-table-column>
-          <el-table-column width="100px" prop="type" label="报名类型"></el-table-column>
-          <el-table-column width="75px" prop="groups" label="组别"></el-table-column>
-          <el-table-column prop="partNo" label="报名编号"></el-table-column>
-          <!-- <el-table-column width="100px" prop="price" label="价格"></el-table-column> -->
+      <el-tab-pane name="child" label="孩子">
+        <el-table class="user-table" stripe :data="childList">
+          <el-table-column prop="childName" label="孩子姓名"></el-table-column>
+          <el-table-column prop="childAge" label="孩子年龄"></el-table-column>
+          <el-table-column prop="childIdCard" label="孩子身份证"></el-table-column>
+          <el-table-column prop="childPhone" label="孩子手机号"></el-table-column>
         </el-table>
       </el-tab-pane>
     </el-tabs>！
@@ -65,22 +38,14 @@
 </template>
 <script>
 const participantMap = {};
-const prices = [120, 180, 80];
 
 export default {
   data() {
     return {
       allList: [],
-      coupeList: [],
-      singleList: [],
-      familyList: [],
-      displayMode: "all",
-      ages: [
-        // { start: 0, end: 10 }
-      ],
-      times: [
-        // { start: "2019-02-25 00:00:00", end: "2019-02-25 23:59:59" },
-      ]
+      parentlist: [],
+      childList: [],
+      displayMode: "all"
     };
   },
   methods: {
@@ -88,14 +53,14 @@ export default {
       if (arr instanceof Array) {
         const refinedData = arr.map(_ => {
           const newDatum = {
-            partNo: _.partNo,
-            type: _.type,
-            name: _.name,
-            phone: _.phone,
-            sex: _.sex,
-            groups: _.groups,
-            age: _.age,
-            idCard: _.idCard
+            parentName: _.name,
+            parentAge: _.age,
+            parentPhone: _.phone,
+            parentIdCard: _.idCard,
+            childName: _.childName,
+            childAge: _.childAge,
+            childPhone: _.childPhone,
+            childIdCard: _.childIdCard
           };
           participantMap[newDatum.idCard] = newDatum;
           return newDatum;
@@ -103,21 +68,14 @@ export default {
 
         refinedData.forEach(_ => {
           this.allList.push(_);
-          if (_.type == "单打") {
-            _.price = prices[1];
-            this.singleList.push(_);
-          } else if (_.type == "双打") {
-            _.price = prices[0];
-            this.coupeList.push(_);
-          } else {
-            _.price = prices[2];
-            this.familyList.push(_);
-          }
+          this.parentlist.push(_);
+          this.childList.push(_);
         });
       }
     },
     getEnrollData(start = 0) {
-      this.$http.post("/joinlist_bm.php", { start }).then(res => {
+      const params = { start };
+      this.$http.get(`/joinlist.php`, { params }).then(res => {
         if (res && res.status == 200 && res.data && res.data.length > 0) {
           this.processData(res.data);
           this.getEnrollData(start + 20);
@@ -126,26 +84,12 @@ export default {
     },
     initTableData() {
       this.allList.splice(0);
-      this.singleList.splice(0);
-      this.coupeList.splice(0);
-      this.familyList.splice(0);
+      this.parentlist.splice(0);
+      this.childList.splice(0);
       this.getEnrollData();
     }
   },
-  computed: {
-    ageStr() {
-      const str = this.ages.reduce((acc, item) => {
-        return acc + `;${item.start},${item.end}`;
-      }, "");
-      return str.substring(1, str.length);
-    },
-    timeStr() {
-      const str = this.times.reduce((acc, item) => {
-        return acc + `;${item.start},${item.end}`;
-      }, "");
-      return str.substring(1, str.length);
-    }
-  },
+  computed: {},
   created() {
     this.initTableData();
   }
--- src/main.js
+++ src/main.js
@@ -1,23 +1,24 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from "vue"
-import App from "./App"
-import axios from "axios"
-import Vaxios from "vue-axios"
-import ElementUI from "element-ui"
-import router from "./router"
+import Vue from "vue";
+import App from "./App";
+import axios from "axios";
+import Vaxios from "vue-axios";
+import ElementUI from "element-ui";
+import router from "./router";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 Vue.use(
   Vaxios,
   axios.create({
     baseURL: "http://njat.kevinlu.cc/lll",
     headers: { "Content-Type": "application/x-www-form-urlencoded" }
+    // headers: { "Content-Type": "application/json" }
   })
-)
+);
 
-Vue.use(ElementUI)
+Vue.use(ElementUI);
 
 /* eslint-disable no-new */
 new Vue({
@@ -25,4 +26,4 @@ new Vue({
   router,
   components: { App },
   template: "<App/>"
-})
+});
