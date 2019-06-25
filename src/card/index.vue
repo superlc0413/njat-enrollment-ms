f81bd19bb45fcee23c16b383828c9f50a798f6ef
Charles Liu <superlc0413@163.com>
2019-06-26 05:21:53 +0800

    夏令营数据汇总

--- src/card/index.vue
+++ src/card/index.vue
@@ -1,36 +1,52 @@
 <template>
   <div>
-    <div class="title tc">亲子跑报名情况</div>
+    <div class="title tc">夏令营报名情况</div>
     <ul class="total-count">
       <li class="row1 tc">共有{{allList.length}}次报名</li>
     </ul>
-    <el-tabs class="main-tab" v-model="displayMode">
-      <el-tab-pane name="all" label="全部">
+    <el-tabs class="main-tab" v-model="currentTab">
+      <el-tab-pane name="all" :label="`全部（${allList.length}）`">
         <el-table class="user-table" stripe :data="allList">
-          <el-table-column prop="parentName" width="110px" label="家长姓名"></el-table-column>
-          <el-table-column prop="parentAge" width="110px" label="家长年龄"></el-table-column>
-          <el-table-column prop="parentIdCard" label="家长身份证"></el-table-column>
-          <el-table-column prop="parentPhone" label="家长手机号"></el-table-column>
-          <el-table-column prop="childName" width="110px" label="孩子姓名"></el-table-column>
-          <el-table-column prop="childAge" width="110px" label="孩子年龄"></el-table-column>
-          <el-table-column prop="childIdCard" label="孩子身份证"></el-table-column>
-          <el-table-column prop="childPhone" label="孩子手机号"></el-table-column>
+          <el-table-column prop="name" width="130px" label="姓名"></el-table-column>
+          <el-table-column prop="age" width="130px" label="年龄"></el-table-column>
+          <el-table-column prop="idCard" label="身份证"></el-table-column>
+          <el-table-column prop="phone" label="手机号"></el-table-column>
+          <el-table-column prop="termName" width="130px" label="期别"></el-table-column>
+          <el-table-column prop="am" width="130px" label="上午课程"></el-table-column>
+          <el-table-column prop="pm" width="130px" label="下午课程"></el-table-column>
         </el-table>
       </el-tab-pane>
-      <el-tab-pane name="parent" label="家长">
-        <el-table class="user-table" stripe :data="parentlist">
-          <el-table-column prop="parentName" label="家长姓名"></el-table-column>
-          <el-table-column prop="parentAge" label="家长年龄"></el-table-column>
-          <el-table-column prop="parentIdCard" label="家长身份证"></el-table-column>
-          <el-table-column prop="parentPhone" label="家长手机号"></el-table-column>
+      <el-tab-pane name="tab1" :label="`第一期（${list1.length}）`">
+        <el-table class="user-table" stripe :data="list1">
+          <el-table-column prop="name" width="130px" label="姓名"></el-table-column>
+          <el-table-column prop="age" width="130px" label="年龄"></el-table-column>
+          <el-table-column prop="idCard" label="身份证"></el-table-column>
+          <el-table-column prop="phone" label="手机号"></el-table-column>
+          <el-table-column prop="termName" width="130px" label="期别"></el-table-column>
+          <el-table-column prop="am" width="130px" label="上午课程"></el-table-column>
+          <el-table-column prop="pm" width="130px" label="下午课程"></el-table-column>
         </el-table>
       </el-tab-pane>
-      <el-tab-pane name="child" label="孩子">
-        <el-table class="user-table" stripe :data="childList">
-          <el-table-column prop="childName" label="孩子姓名"></el-table-column>
-          <el-table-column prop="childAge" label="孩子年龄"></el-table-column>
-          <el-table-column prop="childIdCard" label="孩子身份证"></el-table-column>
-          <el-table-column prop="childPhone" label="孩子手机号"></el-table-column>
+      <el-tab-pane name="tab2" :label="`第二期（${list2.length}）`">
+        <el-table class="user-table" stripe :data="list2">
+          <el-table-column prop="name" width="130px" label="姓名"></el-table-column>
+          <el-table-column prop="age" width="130px" label="年龄"></el-table-column>
+          <el-table-column prop="idCard" label="身份证"></el-table-column>
+          <el-table-column prop="phone" label="手机号"></el-table-column>
+          <el-table-column prop="termName" width="130px" label="期别"></el-table-column>
+          <el-table-column prop="am" width="130px" label="上午课程"></el-table-column>
+          <el-table-column prop="pm" width="130px" label="下午课程"></el-table-column>
+        </el-table>
+      </el-tab-pane>
+      <el-tab-pane name="tab3" :label="`第三期（${list3.length}）`">
+        <el-table class="user-table" stripe :data="list3">
+          <el-table-column prop="name" width="130px" label="姓名"></el-table-column>
+          <el-table-column prop="age" width="130px" label="年龄"></el-table-column>
+          <el-table-column prop="idCard" label="身份证"></el-table-column>
+          <el-table-column prop="phone" label="手机号"></el-table-column>
+          <el-table-column prop="termName" width="130px" label="期别"></el-table-column>
+          <el-table-column prop="am" width="130px" label="上午课程"></el-table-column>
+          <el-table-column prop="pm" width="130px" label="下午课程"></el-table-column>
         </el-table>
       </el-tab-pane>
     </el-tabs>！
@@ -38,14 +54,17 @@
 </template>
 <script>
 const participantMap = {};
+const map1 = ["", "第一期", "第二期", "第三期"];
+const map2 = ["", "游泳", "滑冰", "棒球", "网球", "乒乓球", "足球"];
 
 export default {
   data() {
     return {
       allList: [],
-      parentlist: [],
-      childList: [],
-      displayMode: "all"
+      list1: [],
+      list2: [],
+      list3: [],
+      currentTab: "all"
     };
   },
   methods: {
@@ -53,29 +72,34 @@ export default {
       if (arr instanceof Array) {
         const refinedData = arr.map(_ => {
           const newDatum = {
-            parentName: _.name,
-            parentAge: _.age,
-            parentPhone: _.phone,
-            parentIdCard: _.idCard,
-            childName: _.childName,
-            childAge: _.childAge,
-            childPhone: _.childPhone,
-            childIdCard: _.childIdCard
+            name: _.name,
+            age: _.age,
+            phone: _.phone,
+            idCard: _.idCard,
+            termCode: _.period,
+            termName: map1[_.period],
+            am: map2[_.forenoon],
+            pm: map2[_.afternoon]
           };
-          participantMap[newDatum.idCard] = newDatum;
+          // participantMap[newDatum.idCard] = newDatum;
           return newDatum;
         });
 
         refinedData.forEach(_ => {
+          if (_.termCode == 1) {
+            this.list1.push(_);
+          } else if (_.termCode == 2) {
+            this.list2.push(_);
+          } else if (_.termCode == 3) {
+            this.list3.push(_);
+          }
           this.allList.push(_);
-          this.parentlist.push(_);
-          this.childList.push(_);
         });
       }
     },
     getEnrollData(start = 0) {
       const params = { start };
-      this.$http.get(`/joinlist.php`, { params }).then(res => {
+      this.$http.get(`/joinlist2.php`, { params }).then(res => {
         if (res && res.status == 200 && res.data && res.data.length > 0) {
           this.processData(res.data);
           this.getEnrollData(start + 20);
@@ -84,8 +108,9 @@ export default {
     },
     initTableData() {
       this.allList.splice(0);
-      this.parentlist.splice(0);
-      this.childList.splice(0);
+      this.list1.splice(0);
+      this.list2.splice(0);
+      this.list3.splice(0);
       this.getEnrollData();
     }
   },
